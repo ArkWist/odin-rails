e4b748fc74b6f3cd44b65d5131021399e938fb87
Daniel A. G. Lilley <arkwist@gmail.com>
2016-07-22 21:29:44 +0900

    Add number guessing

--- project_sinatra/web_guesser/web_guesser.rb
+++ project_sinatra/web_guesser/web_guesser.rb
@@ -1,8 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(0..100)
+number = rand(100)
+
+def check_guess(guess, number)
+  if guess.nil?
+    message = ""
+  else
+    message = if guess.to_i == number then "You got it right!"
+              elsif guess.to_i > number + 5 then "Way too high!"
+              elsif guess.to_i > number then "Too high!"
+              elsif guess.to_i < number - 5 then "Way too low!"
+              elsif guess.to_i < number then "Too low!"
+              else "" end
+  end
+  message
+end
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"]
+  message = check_guess(guess, number)
+  erb :index, :locals => {:number => number, :message => message}
 end
+
