4a203333f82defe4125ab498a44c20890e08ed42
Daniel A. G. Lilley <arkwist@gmail.com>
2016-07-22 22:07:02 +0900

    Change background depending on guess accuracy

--- project_sinatra/web_guesser/web_guesser.rb
+++ project_sinatra/web_guesser/web_guesser.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 number = rand(100)
+#@@remaining = 0
 
 def check_guess(guess, number)
   message = if !guess.to_i.to_s == guess then ""
@@ -9,9 +10,16 @@ def check_guess(guess, number)
             else "#{(guess.to_i - number).abs > 5 ? "Way too" : "Too"} #{guess.to_i - number > 0 ? "high" : "low"}." end
 end
 
+def check_background(guess, number)
+  background = if !guess.to_i.to_s == guess then ""
+               elsif guess.to_i == number then "#0F0"
+               else (guess.to_i - number).abs > 5 ? "#F33" : "#F93" end
+end
+
 get '/' do
   guess = params["guess"]
   message = check_guess(guess, number)
-  erb :index, :locals => {:number => number, :message => message}
+  background = check_background(guess, number)
+  erb :index, :locals => {:number => number, :message => message, :background => background}
 end
 
