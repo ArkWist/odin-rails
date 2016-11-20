3389ff6b8b4e3f9b27a1d3fd5b98b58836821202
ArkWist <arkwist@gmail.com>
2016-11-20 22:07:07 +0900

    Fix formatting

--- project_sql_database/tutorial_0.sql
+++ project_sql_database/tutorial_0.sql
@@ -1,16 +1,16 @@
 -- SELECT basics
 
--- 1. Show the population of Germany
+-- 1. Show the population of Germany.
 SELECT population
   FROM world
  WHERE name = 'Germany';
   
--- 2. Show the name and the population for 'Ireland', 'Iceland' and 'Denmark'
+-- 2. Show the name and the population for Ireland, Iceland and Denmark.
 SELECT name, population
   FROM world
  WHERE name IN ('Ireland', 'Iceland', 'Denmark');
 
--- 3. Show the country and the area for countries with an area between 200,000 and 250,000
+-- 3. Show the country and the area for countries with an area between 200,000 and 250,000.
 SELECT name, area
   FROM world
  WHERE area BETWEEN 200000
