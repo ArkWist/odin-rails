97a73c0e7273970e5a07049feef0232597ccbc02
ArkWist <arkwist@gmail.com>
2016-11-20 21:48:31 +0900

    Complete tutorial 0

--- project_sql_database/tutorial_0.sql
+++ project_sql_database/tutorial_0.sql
@@ -1,18 +1,17 @@
+-- SELECT basics
 
--- Modify it to show the population of Germany
+-- 1. Show the population of Germany
 SELECT population
   FROM world
- WHERE name = 'Germany'
+ WHERE name = 'Germany';
   
--- Show the name and the population for 'Ireland', 'Iceland' and 'Denmark'.
+-- 2. Show the name and the population for 'Ireland', 'Iceland' and 'Denmark'
 SELECT name, population
   FROM world
  WHERE name IN ('Ireland', 'Iceland', 'Denmark');
 
--- Modify it to show the country and the area for countries with an area between 200,000 and 250,000. 
+-- 3. Show the country and the area for countries with an area between 200,000 and 250,000
 SELECT name, area
   FROM world
  WHERE area BETWEEN 200000
-                AND 250000
-
-
+                AND 250000;
