f3e948702ca603153999a81a4577755240f52542
ArkWist <arkwist@gmail.com>
2016-11-28 23:12:52 +0900

    Do tutorial 9 up to 3

--- project_sql_database/tutorial_9.sql
+++ project_sql_database/tutorial_9.sql
@@ -4,9 +4,18 @@
 SELECT COUNT(stops.id)
   FROM stops;
 
--- 2. 
+-- 2. Find the id value for the stop 'Craiglockhart'.
+SELECT stops.id
+  FROM stops
+ WHERE stops.name = 'Craiglockhart';
 
+-- 3. Give the id and the name for the stops on the '4' 'LRT' service.
+SELECT stops.id, stops.name
+  FROM stops
+  JOIN route ON stops.id = route.stop
+ WHERE route.company = 'LRT' AND route.num = '4';
 
+-- 4. Give the number of routes that visit either London Road (149) or Craiglockhart (53)
 
 
 
