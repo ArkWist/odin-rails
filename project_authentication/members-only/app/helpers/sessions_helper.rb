f7632c5c3a706bf5d92cd552f91255c297064dd2
daglilley <daglilley@gmail.com>
2017-05-27 13:40:14 +0900

    Make working login and logout

--- project_authentication/members-only/app/helpers/sessions_helper.rb
+++ project_authentication/members-only/app/helpers/sessions_helper.rb
@@ -1,2 +1,27 @@
 module SessionsHelper
+  
+  def log_in(user)
+    remember_token = User.new_token
+    cookies.permanent[:remember_token] = remember_token
+    session[:user_id] = user.id
+    @current_user = user
+  end
+  
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  
+  def current_user=(user)
+    @current_user = user
+  end
+ 
+ 
+  def log_out
+    session.delete(:user_id)
+    cookies.delete(:remember_token) #Delete cookies[:remember_token]
+    @current_user = nil
+  end
+  
+  
 end
--- project_authentication/members-only/config/routes.rb
+++ project_authentication/members-only/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get  '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  root 'sessions#new'
+  
+  get  '/login',    to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#delete'
   
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
