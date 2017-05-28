5ea962ce3de003520f9d04f7cb022743332f505a
daglilley <daglilley@gmail.com>
2017-05-28 15:43:09 +0900

    Complete posting functionality

--- project_authentication/members-only/config/routes.rb
+++ project_authentication/members-only/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+=begin
   #get 'post/new'
   #get 'post/create'
   
@@ -8,10 +10,27 @@ Rails.application.routes.draw do
   root 'posts#index'
   #root 'sessions#new'
   
-  get  '/login',    to: 'sessions#new'
-  post '/login',    to: 'sessions#create'
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#delete'
+  
+  get  '/post', to: 'posts#new'
+  post '/post', to: 'posts#create'
   
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+=end
+
+  root 'posts#index'
+  
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#delete'
+  
+  get  '/newpost', to: 'posts#new'
+  post '/newpost', to: 'posts#create'
+  
+  resources :users
+  resources :posts, only: [:new, :create, :index]
+
 end
