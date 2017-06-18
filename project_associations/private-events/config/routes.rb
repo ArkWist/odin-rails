7e726df57d07cce92dd66392a11bf616b42a0e59
daglilley <daglilley@gmail.com>
2017-06-18 23:50:23 +0900

    Create User model and controller

--- project_associations/private-events/config/routes.rb
+++ project_associations/private-events/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  #get 'users/new'
+  get 'users/create'
+  get 'users/show'
+
+  get '/signup', to: 'users#new'
+
+  resources :users
+
+  #get  '/login', to: 'users#new'
+  #post '/login', to: 'users#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
