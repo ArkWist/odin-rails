cd037b02a51f456d12feb5e97681151acc8e71c8
daglilley <daglilley@gmail.com>
2017-06-20 23:07:13 +0900

    Add events routes

--- project_associations/private-events/config/routes.rb
+++ project_associations/private-events/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  
+  
+  get 'events/new'
+  post '/events/new', to: 'events#create'
+  get 'events/show'
+  get 'events/index'
+
   get    '/login',  to: 'sessions#new'
   post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
