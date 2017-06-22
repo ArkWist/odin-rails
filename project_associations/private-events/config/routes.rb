4943dac94b24ae80f0327773a11080b2b2e72cd2
daglilley <daglilley@gmail.com>
2017-06-22 17:27:49 +0900

    Make events#new use default path

--- project_associations/private-events/config/routes.rb
+++ project_associations/private-events/config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
   
   
-  get  '/newevent', to: 'events#new'
-  post '/newevent', to: 'events#create'
+  #get  '/newevent', to: 'events#new'
+  #post '/newevent', to: 'events#create'
   get 'events/show'
   get 'events/index'
+  
+  get  '/events/new', to: 'events#new'
+  post '/events/new', to: 'events#create'
+  
+  # MAKE NEWEVENT USE /events/new
 
-  get    '/login',  to: 'sessions#new'
-  post   '/login',  to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get    'login',  to: 'sessions#new'
+  post   'login',  to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   #get 'users/new'
   get 'users/create'
