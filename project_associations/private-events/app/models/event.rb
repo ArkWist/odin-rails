aa0fbe98531f1256be8902582bcae145634bc792
daglilley <daglilley@gmail.com>
2017-06-26 20:32:50 +0900

    Remove attendee from Event

--- project_associations/private-events/app/models/event.rb
+++ project_associations/private-events/app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator,  class_name: "User"
+  
   has_many   :attendees, class_name: "User", through: :invites
   has_many   :invites, foreign_key: :attended_event_id, dependent: :destroy
   
--- project_associations/private-events/db/schema.rb
+++ project_associations/private-events/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623011821) do
+ActiveRecord::Schema.define(version: 20170626113209) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
