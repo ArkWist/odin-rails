7b0813e0d1278a5e02023319d22a4c286454d696
daglilley <daglilley@gmail.com>
2017-07-01 14:47:51 +0900

    Write tests for User model

--- project_associations/private-events/app/models/user.rb
+++ project_associations/private-events/app/models/user.rb
@@ -3,8 +3,22 @@ class User < ApplicationRecord
   has_many :invites, foreign_key: :attendee_id, dependent: :destroy
   has_many :attended_events, class_name: "Event", through: :invites
   
-  validates :name, presence: true,
-                   uniqueness: { case_sensitive: true }
-
+  before_save { self.email = email.downcase }
   
+  validates :name,  presence:   true, length: { maximum: 64 },
+                    uniqueness: { case_sensitive: true }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence:   true, length: { maximum: 256 },
+                    format:     { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? \
+      BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
 end
