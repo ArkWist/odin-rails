e91466ebfbd9927792b3babec1e36cd7af1d7b3d
daglilley <daglilley@gmail.com>
2017-07-01 10:33:50 +0900

    Add home

--- project_associations/private-events/config/routes.rb
+++ project_associations/private-events/config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/home'
+
+  root 'static_pages#home'
+
   get    'login',  to: 'sessions#new'
   post   'login',  to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
   get    '/signup', to: 'users#new'
   post   '/signup', to: 'users#create'
-  get    'users/show'
 
   resources :users
   resources :events do
-    resources :invite
+    resources :invite, only: [:create, :destroy]
   end
 
 end
