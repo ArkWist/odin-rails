e4f472bb66d1b2fc8147136cebd473c8673a8aab
daglilley <daglilley@gmail.com>
2017-07-02 22:55:10 +0900

    Add event editing and deletion

--- project_associations/private-events/app/controllers/events_controller.rb
+++ project_associations/private-events/app/controllers/events_controller.rb
@@ -24,11 +24,25 @@ class EventsController < ApplicationController
   def destroy
     @event.destroy
     flash[:success] = "Event deleted"
-    redirect_to request.referrer || root_url
+    redirect_to @current_user
   end
 
   def index
     @events = Event.all
+  end
+  
+  def edit
+    @event = Event.find(params[:id])
+  end
+  
+  def update
+    @event = Event.find(params[:id])
+    if @event.update_attributes(event_params)
+      flash[:success] = "Event updated"
+      redirect_to @event
+    else
+      render 'edit'
+    end
   end
   
   private
