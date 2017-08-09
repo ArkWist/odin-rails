ec69d9a24f607ac09b5411319211ad5c24d66910
daglilley <daglilley@gmail.com>
2017-08-09 14:36:07 +0900

    Design flight search form

--- project_adv_forms/flight-booker/app/controllers/flights_controller.rb
+++ project_adv_forms/flight-booker/app/controllers/flights_controller.rb
@@ -3,5 +3,8 @@ class FlightsController < ApplicationController
   end
 
   def index
+    @airports        = Airport.all.map{ |a| a.code }
+    @flight_dates    = Flight.all.map{ |f| f.start_time.to_date }
+    @passenger_count = 1..4
   end
 end
