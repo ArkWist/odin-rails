6277fd19cc58d19b70f4babd66e84a3bc5db5137
daglilley <daglilley@gmail.com>
2017-08-11 17:21:18 +0900

    Show searched for flights

--- project_adv_forms/flight-booker/app/controllers/flights_controller.rb
+++ project_adv_forms/flight-booker/app/controllers/flights_controller.rb
@@ -8,9 +8,16 @@ class FlightsController < ApplicationController
     @passenger_count = 1..4
     
     if params[:flight]
-      @flights = Flight.where(origin_id:      params[:flight][:origin],
-                              destination_id: params[:flight][:destination],
-                              start_time:     params[:flight][:date].to_date)
+      @flights = Flight.where(origin_id:      params[:flight][:origin_id],
+                              destination_id: params[:flight][:destination_id],
+                              start_time:     params[:flight][:date].to_date.beginning_of_day..params[:flight][:date].to_date.end_of_day)
+                              
+                              #'start_time BETWEEN ? AND ?', params[:flight][:date].beginning_of_day, params[:flight][:date].end_of_day)
+                              
+                              
+                              
+                              
+                              #start_time:     params[:flight][:date].to_date)
       puts "DATETIME: #{params[:flight][:date].to_date}"
       puts "FLIGHT DATA: #{Flight.first.start_time}"
     end
