0065249e31beb2e8a2dbd598272aa86fc0c4199c
daglilley <daglilley@gmail.com>
2017-09-13 23:02:05 +0900

    Create sidebar

--- project_apis/flickr-feed/app/helpers/flickr_helper.rb
+++ project_apis/flickr-feed/app/helpers/flickr_helper.rb
@@ -5,12 +5,23 @@ module FlickrHelper
     flickr.photos.search(:user_id => user_id).first(photo_count)
   end
   
+  def get_name_id(username)
+    begin
+      flickr.people.findByUsername(:username => username)     
+    rescue => exception
+      render :partial => '/flickr/unavailable'
+    end
+  end
+  
   def render_flickr_sidebar_widget(user_id, photo_count = 12, columns = 2)
     begin
+      puts "step 1"
       photos = user_photos(user_id, photo_count).in_groups_of(columns)
+      puts "step 2"
       render :partial => '/flickr/sidebar_widget',
              :locals  => { :photos => photos }
     rescue Exception
+      puts "step 3"
       render :partial => '/flickr/unavailable'
     end
   end
