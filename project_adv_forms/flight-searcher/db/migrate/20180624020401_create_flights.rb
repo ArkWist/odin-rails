b760da73843c124dfefac649ef5c35d8fca9c811
daglilley <daglilley@gmail.com>
2018-06-24 21:56:17 +0900

    Potentially fix foreign key error

--- project_adv_forms/flight-searcher/db/migrate/20180624020401_create_flights.rb
+++ project_adv_forms/flight-searcher/db/migrate/20180624020401_create_flights.rb
@@ -1,8 +1,8 @@
 class CreateFlights < ActiveRecord::Migration[5.2]
   def change
     create_table :flights do |t|
-      t.references :origin, foreign_key: true
-      t.references :destination, foreign_key: true
+      t.references :origin
+      t.references :destination
       t.datetime :departure
       t.datetime :arrival
 
